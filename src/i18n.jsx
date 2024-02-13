2110c9a194f87d6e00172cb8919f85d655dd525b
rootknight <ljmroot@gmail.com>
2024-02-13 14:12:34 +0800

    默认中文

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -2,10 +2,10 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import languages from "./locales/index.jsx";
 
-var language = localStorage.getItem("language") || "en"
+var language = localStorage.getItem("language") || "zh";
 
 if (!languages[language]) {
-	language = "en"
+  language = "zh";
 }
 
 export function getLanguage() {
@@ -13,26 +13,24 @@ export function getLanguage() {
 }
 
 export function setLanguage(newLanguage) {
-	language = newLanguage;
-	localStorage.setItem("language", language);
-	i18n.changeLanguage(language);
-};
+  language = newLanguage;
+  localStorage.setItem("language", language);
+  i18n.changeLanguage(language);
+}
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-        en: languages.en,
-        es: languages.es,
-        fr: languages.fr,
-        zh: languages.zh,
-        de: languages.de,
-    },
-    lng: language,
-    fallbackLng: "en",
-    interpolation: {
-        escapeValue: false,
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources: {
+    en: languages.en,
+    es: languages.es,
+    fr: languages.fr,
+    zh: languages.zh,
+    de: languages.de,
+  },
+  lng: language,
+  fallbackLng: "zh",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
